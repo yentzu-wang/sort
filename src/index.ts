c53af51c580abdaeddf30f152673d29ead342154
Frank Wang <frank7300228@gmail.com>
2022-05-16 23:06:45 +0800

    Finish 87

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,8 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { Sorter } from "./Sorter"
+import { NumbersCollection } from "./NumbersCollection"
 
-  sort(): void {
-    const { length } = this.collection
+const numberCollection = new NumbersCollection([10, 3, -5, 0])
+const sorter = new Sorter(numberCollection)
 
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // All of this only works if collection is number[]
-        // If collection is an array of numbers
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j]
-            this.collection[j] = this.collection[j + 1]
-            this.collection[j + 1] = leftHand
-          }
-        }
-
-        // Only going to work if collection is a string
-        // If collection is a string, do this logic instead:
-        if (typeof this.collection === "string") {
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0])
 sorter.sort()
-console.log(sorter.collection)
+console.log(numberCollection.data)
