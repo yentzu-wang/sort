b4310d9f4b446a9a51e595c8e012ec9e84140a3c
Frank Wang <frank7300228@gmail.com>
2022-05-17 00:46:48 +0800

    Finish 93

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter"
 import { NumbersCollection } from "./NumbersCollection"
 import { CharactersCollection } from "./CharactersCollection"
+import { LinkedList } from "./LinkedList"
 
 // const numberCollection = new NumbersCollection([10, 3, -5, 0])
 // const sorter = new Sorter(numberCollection)
@@ -8,8 +9,18 @@ import { CharactersCollection } from "./CharactersCollection"
 // sorter.sort()
 // console.log(numberCollection.data)
 
-const charactersCollection = new CharactersCollection("Xaayb")
-const sorter = new Sorter(charactersCollection)
+// const charactersCollection = new CharactersCollection("Xaayb")
+// const sorter = new Sorter(charactersCollection)
 
+// sorter.sort()
+// console.log(charactersCollection.data)
+
+const linkedList = new LinkedList()
+linkedList.add(500)
+linkedList.add(-10)
+linkedList.add(-3)
+linkedList.add(4)
+
+const sorter = new Sorter(linkedList)
 sorter.sort()
-console.log(charactersCollection.data)
+linkedList.print()
