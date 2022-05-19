5bd599095475e6bc965ee7fc35034c84e209c155
Frank Wang <frank7300228@gmail.com>
2022-05-19 10:34:02 +0800

    Finish 99

--- build/index.js
+++ build/index.js
@@ -1,20 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Sorter_1 = require("./Sorter");
 const LinkedList_1 = require("./LinkedList");
 // const numberCollection = new NumbersCollection([10, 3, -5, 0])
-// const sorter = new Sorter(numberCollection)
-// sorter.sort()
+// numberCollection.sort()
 // console.log(numberCollection.data)
 // const charactersCollection = new CharactersCollection("Xaayb")
-// const sorter = new Sorter(charactersCollection)
-// sorter.sort()
+// charactersCollection.sort()
 // console.log(charactersCollection.data)
 const linkedList = new LinkedList_1.LinkedList();
 linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
-const sorter = new Sorter_1.Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,11 @@ import { CharactersCollection } from "./CharactersCollection"
 import { LinkedList } from "./LinkedList"
 
 // const numberCollection = new NumbersCollection([10, 3, -5, 0])
-// const sorter = new Sorter(numberCollection)
-
-// sorter.sort()
+// numberCollection.sort()
 // console.log(numberCollection.data)
 
 // const charactersCollection = new CharactersCollection("Xaayb")
-// const sorter = new Sorter(charactersCollection)
-
-// sorter.sort()
+// charactersCollection.sort()
 // console.log(charactersCollection.data)
 
 const linkedList = new LinkedList()
@@ -21,6 +17,5 @@ linkedList.add(-10)
 linkedList.add(-3)
 linkedList.add(4)
 
-const sorter = new Sorter(linkedList)
-sorter.sort()
+linkedList.sort()
 linkedList.print()
